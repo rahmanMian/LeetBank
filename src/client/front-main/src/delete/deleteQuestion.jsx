6568458c622b8270a590271fb2aaef82856d58b6
Usman Khan <muhukhan@umass.edu>
2024-05-25 05:23:49 -0400

    first commit yayyy great success!

--- src/client/front-main/src/delete/deleteQuestion.jsx
+++ src/client/front-main/src/delete/deleteQuestion.jsx
@@ -4,6 +4,7 @@ import "../App.css";
  * Deletes a question from the state based on its ID.
  *
  * @function DeleteQuestion
+ * @author Rahman Mian
  * @param {Object} params - The parameters object.
  * @param {string} id - The ID of the question to be deleted.
  * @param {Function} setQuestion - The state setter function residing in addQuestion for updating the questions state.
