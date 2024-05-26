d61d0048359d990e9900a4adb8652e7e8ae211f1
rahmanMian <rahmanakbarmian@gmail.com>
2024-05-26 06:24:43 -0400

    added react bootstrap

--- src/client/front-main/src/add/block.jsx
+++ src/client/front-main/src/add/block.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../App.css';
 import { DeleteQuestion } from '../delete/deleteQuestion';
+import Accordion from 'react-bootstrap/Accordion';
 
 /**
  * Renders a block representing a question with a delete button.
@@ -24,6 +25,20 @@ export function Block({ question, setQuestion, setIndex }) {
         <div className="block" id={question.id}>
             <span>{question.title}</span>
             <button onClick={handleDeleteQuestion} >Delete</button>
+            <Accordion>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      </Accordion>
         </div>
     );
 }
