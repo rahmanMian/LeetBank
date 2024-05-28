15a09fbf15e3fb4f341286157bb38d6e412a5e4d
rahmanMian <rahmanakbarmian@gmail.com>
2024-05-28 07:44:58 -0400

    making accrodion myself might jsut be  better

--- src/client/front-main/src/add/block.jsx
+++ src/client/front-main/src/add/block.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure Bootstrap CSS is imported
 import '../App.css';
 import { DeleteQuestion } from '../delete/deleteQuestion';
-import { Accordion } from 'react-bootstrap';
+
 
 
 /**
@@ -27,20 +27,6 @@ export function Block({ question, setQuestion, setIndex }) {
             <div className="block" id={question.id}>
                 <span>{question.title}</span>
                 <button onClick={handleDeleteQuestion}>Delete</button>
-                <Accordion defaultActiveKey="0" >
-                <Accordion.Item eventKey="0">
-                    <Accordion.Header>Accordion Item #1</Accordion.Header>
-                    <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.
-                    </Accordion.Body>
-                </Accordion.Item>
-            </Accordion>
             </div>
           
         </>
